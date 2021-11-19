b7af7730babd5a8bedfc339579ce87f2c50c099f
Clayton Hiebert <clayton.hiebert1@gmail.com>
2021-11-19 16:54:57 -0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import Main from './components/MainComponent';
+import {
+    BrowserRouter
+} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
 
     render() {
-        return (
-            <div className="App">
-                <Main />
-            </div>
+        return ( <
+            BrowserRouter >
+            <
+            div className = "App" >
+            <
+            Main / >
+            <
+            /div> < /
+            BrowserRouter >
         );
     };
 }
